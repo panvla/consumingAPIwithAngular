f81d7adb3378b96c4eef2f6e87197d2ece3451cb
panvla <pandvlado@gmail.com>
2023-03-23 06:22:44 +0100

    added service implementation and initial project.

---
+++ userapicatalog/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { UserdetailComponent } from './component/userdetail/userdetail.component';
+import { UsersComponent } from './component/users/users.component';
+
+const routes: Routes = [
+  { path: 'users', component: UsersComponent },
+  { path: 'user/:uuid', component: UserdetailComponent },
+  { path: '**', redirectTo: 'users' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
