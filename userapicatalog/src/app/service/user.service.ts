a5501809b286f4d74dfc589740d799ead0e790cb
panvla <pandvlado@gmail.com>
2023-03-23 07:06:44 +0100

    done mapping the users.

--- userapicatalog/src/app/service/user.service.ts
+++ userapicatalog/src/app/service/user.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
+import { User } from '../interface/user.interface';
+import { Response } from '../interface/response.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -12,11 +14,40 @@ export class UserService {
 
   // Fetch users
   getUsers(size: number = 10): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/?results=${size}`);
+    return this.http
+      .get<any>(`${this.apiUrl}/?results=${size}`)
+      .pipe(map((response) => this.processResponse(response)));
   }
 
   // Fetch one user using the user UUID.
   getUser(uuid: number = 1): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/?uuid=${uuid}`);
+    return this.http
+      .get<any>(`${this.apiUrl}/?uuid=${uuid}`)
+      .pipe(map((response) => this.processResponse(response)));
+  }
+
+  private processResponse(response: Response): Response {
+    return {
+      info: { ...response.info },
+      results: response.results.map(
+        (user: any) =>
+          <User>{
+            uuid: user.login.uuid,
+            firstName: user.name.first,
+            lastName: user.name.last,
+            email: user.email,
+            username: user.login.username,
+            gender: user.gender,
+            address: `${user.location.street.number} ${user.location.street.name} ${user.location.city}, ${user.location.country}`,
+            dateOfBirth: user.dob.date,
+            phone: user.phone,
+            imageUrl: user.picture.medium,
+            coordinate: {
+              latitude: +user.location.coordinates.latitude,
+              longitude: +user.location.coordinates.longitude,
+            },
+          }
+      ),
+    };
   }
 }
