bf9b5a97607f27171fc9e0cfbd5ad60722c25699
panvla <pandvlado@gmail.com>
2023-03-23 09:14:45 +0100

    added route resolver.

--- userapicatalog/src/app/app-routing.module.ts
+++ userapicatalog/src/app/app-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserdetailComponent } from './component/userdetail/userdetail.component';
 import { UsersComponent } from './component/users/users.component';
+import { UserResolver } from './service/user.resolver';
 
 const routes: Routes = [
   { path: 'users', component: UsersComponent },
-  { path: 'user/:uuid', component: UserdetailComponent },
+  {
+    path: 'user/:uuid',
+    component: UserdetailComponent,
+    resolve: { resolvedResponse: UserResolver },
+  },
   { path: '**', redirectTo: 'users' },
 ];
 
--- userapicatalog/src/app/component/userdetail/userdetail.component.ts
+++ userapicatalog/src/app/component/userdetail/userdetail.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
-import { Response } from 'src/app/interface/response.interface';
 import { User } from 'src/app/interface/user.interface';
 import { UserService } from 'src/app/service/user.service';
 
@@ -10,7 +9,6 @@ import { UserService } from 'src/app/service/user.service';
   styleUrls: ['./userdetail.component.css'],
 })
 export class UserdetailComponent implements OnInit {
-  response: Response;
   user: User;
   mode: 'edit' | 'locked' = 'locked';
   buttonText: 'Save Changes' | 'Edit' = 'Edit';
@@ -21,16 +19,20 @@ export class UserdetailComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.activatedRoute.paramMap.subscribe((params: ParamMap) => {
-      console.log('User ID: ' + params.get('uuid'));
-      this.userService
-        .getUser(params.get('uuid'))
-        .subscribe((response: any) => {
-          console.log(response);
-          this.response = response;
-          this.user = this.response.results[0];
-        });
-    });
+    this.user = <User>(
+      this.activatedRoute.snapshot.data['resolvedResponse'].results[0]
+    );
+    console.log(this.user);
+    // this.activatedRoute.paramMap.subscribe((params: ParamMap) => {
+    //   console.log('User ID: ' + params.get('uuid'));
+    //   this.userService
+    //     .getUser(params.get('uuid'))
+    //     .subscribe((response: any) => {
+    //       console.log(response);
+    //       this.response = response;
+    //       this.user = this.response.results[0];
+    //     });
+    // });
   }
 
   changeMode(mode: 'edit' | 'locked'): void {
